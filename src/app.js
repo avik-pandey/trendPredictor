823ac6babe748e94b256fe5987f2567bee38f528
Avik Pandey <avikpandey1@gmail.com>
2020-08-04 11:28:03 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
 const path = require('path')
 const express = require('express')
 const ejs = require('ejs')
+var mongoose = require('mongoose');
+
+const url = "mongodb://user:flipkart@cluster0-shard-00-00.pe7lw.mongodb.net:27017,cluster0-shard-00-01.pe7lw.mongodb.net:27017,cluster0-shard-00-02.pe7lw.mongodb.net:27017/intelFashion?ssl=true&replicaSet=atlas-r398qc-shard-0&authSource=admin&retryWrites=true&w=majority";
+// var db = 'intelFashion'; 
+
+mongoose.connect(url, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+
+})
+.then(() => {
+      console.log('Connection to the Atlas Cluster is successfuul!');
+  })
+.catch((err) => console.error(err));
+
+var instaSchema = new mongoose.Schema({
+    hashtag:  String , 
+    imgUrl: String
+    
+  });
+  
+  
+  
+var insta = mongoose.model("insta", instaSchema);
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -41,3 +65,5 @@ app.get('/pinterest', (req, res) => {
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
   })
+
+
